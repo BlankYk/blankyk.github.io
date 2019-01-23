23cf1968ec86369509aa50939b64ac7e4f825dc4
BlankYk <917885215@qq.com>
2019-01-24 01:15:10 +0800

    Site updated: 2019-01-24 01:15:07

--- js/loading_time.js
+++ js/loading_time.js
@@ -1,4 +1,4 @@
  window.onload = function () {
         var loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart; 
-        console.log('本次页面加载时间： '+ loadTime);
+        console.log('本次页面加载时间： '+ loadTime+'ms');
     }
