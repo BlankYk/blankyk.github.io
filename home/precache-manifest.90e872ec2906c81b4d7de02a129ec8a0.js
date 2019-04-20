02b655314c2b01fd265780eb64cc04003f641abc
BlankYk <917885215@qq.com>
2019-04-20 14:31:56 +0800

    Site updated: 2019-04-20 14:31:52

---
+++ home/precache-manifest.90e872ec2906c81b4d7de02a129ec8a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "58d0f3247d8d4a441ab9",
+    "url": "./static/css/main.79a7203d.chunk.css"
+  },
+  {
+    "revision": "58d0f3247d8d4a441ab9",
+    "url": "./static/js/main.5d396bb8.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "5caae218e0d19787b808",
+    "url": "./static/css/2.6146742e.chunk.css"
+  },
+  {
+    "revision": "5caae218e0d19787b808",
+    "url": "./static/js/2.af43cda5.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdf1a72dcb50b6f2ae3b1610f290bc9e",
+    "url": "./index.html"
+  }
+];
